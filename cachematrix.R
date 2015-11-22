19b97744033cbc915469c35f4628581ac32326e1
AJ Hall <ajhall98@gmail.com>
2015-11-22 17:05:45 -0500

    updated for Coursera homework assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,73 @@
-## Put comments here that give an overall description of what your
-## functions do
+# =====================================================
+# cachematrix.R
+# Author: AJ Hall
+# Create date:  11/22/15
+#
+#-------------------------------------
+# makeCacheMatrix ()
+#
+# you are setting up a cacheMatrix
+# takes a normal matrix as input# Calling Instructions:
+# -----------------
+# Run this first to setup a cached matrix
+#
+# call example >>      mycMatrix1 <- makeCacheMatrix(myNormalMatrix)
+#
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(matrixPassedIn = matrix()) {  # This function is called only ONCE
 
+
+     my_cached_matrix <- NULL                  # this initializes the cache
+
+     set <- function(matrixToInvert) {
+          matrixPassedIn <<- matrixToInvert
+          my_cached_matrix <<- NULL
+     }
+
+     get <- function() matrixPassedIn                                   # these are functions
+     invertMatrix <- function(invMatx) my_cached_matrix <<- invMatx     # we will pass back
+     getInvertedMatrix <- function() my_cached_matrix                   # they are used by the other
+     # function
+     list(set = set, get = get,
+          invertMatrix = invertMatrix,
+          getInvertedMatrix = getInvertedMatrix)
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+
+
+#-------------------------------------
+# cacheSolve ()
+#
+# input parameter to this function is the cached Matrix instantiated with the above function
+#
+# call example >>       cacheSolve(mycMatrix1)
+# -----------------
+
+cacheSolve <- function(matrixPasssedIn, ...) {
+
+     myInvertedMatrix <- matrixPasssedIn$getInvertedMatrix()   # see if there is a cached version
+
+     if(!is.null(myInvertedMatrix)) {                    # if there is a value in the cache
+          message("getting inverted, cached matrix")     # then print the msg, return it and
+          return(myInvertedMatrix)                       # jump out with the answer
+     }
+
+     # -------------------------------------------
+     # if no cached version, then do the stuff below
+
+     data <- matrixPasssedIn$get()           # pass the Vector entered to internal "data" vector
+
+     myInvertedMatrix <- solve(data, ...)    # compute the mean first time
+
+     matrixPasssedIn$invertMatrix(myInvertedMatrix)           # call the invertMatrix function
+     # basically just passses out what we
+     # just computed
+     myInvertedMatrix
 }
+
+#--------------------
+# eof cachematrix.R
